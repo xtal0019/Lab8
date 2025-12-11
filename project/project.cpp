37747ebda922478666b6175b463dc71917c1d6cc
Xtal0019 <dmitriyp709@gmail.com>
2025-12-11 12:36:45 +0300

    Добавлен форматированный вывод в консоль

--- project/project.cpp
+++ project/project.cpp
@@ -17,18 +17,24 @@ int main()
     try
     {
         read("data.txt", partic, size);
+        cout << "***** Марафон *****\n\n";
         for (int i = 0; i < size; i++)
         {
+            cout << "Номер участника: ";
             cout << partic[i]->numb << '\n';
-            cout << partic[i]->reader.last_name << '\n';
-            cout << partic[i]->reader.first_name << '\n';
-            cout << partic[i]->reader.middle_name << '\n';
-            cout << partic[i]->start.hour << ' ';
-            cout << partic[i]->start.minutes << ' ';
+            cout << "Участник: ";
+            cout << partic[i]->reader.last_name << ' ';
+            cout << partic[i]->reader.first_name[0] << ". ";
+            cout << partic[i]->reader.middle_name[0] << ".\n";
+            cout << "Время старта: ";
+            cout << partic[i]->start.hour << ':';
+            cout << partic[i]->start.minutes << ':';
             cout << partic[i]->start.seconds << '\n';
-            cout << partic[i]->finish.hour << ' ';
-            cout << partic[i]->finish.minutes << ' ';
+            cout << "Время финиша: ";
+            cout << partic[i]->finish.hour << ':';
+            cout << partic[i]->finish.minutes << ':';
             cout << partic[i]->finish.seconds << '\n';
+            cout << "Название клуба: ";
             cout << partic[i]->club << '\n';
             cout << '\n';
         }
