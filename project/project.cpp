7727284de713fe25e75a84a4f17de110c963daaa
xtal0019 <xtal0019@gmail.com>
2025-12-11 10:07:43 +0300

    Update project.cpp
    Добавлен вывод общей информации о задании

--- project/project.cpp
+++ project/project.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #1\n";
+    cout << "Author: Petrovski Dmitry\n";
     return 0;
 }
