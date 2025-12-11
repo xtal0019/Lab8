bbf71e215fa0853b158f357dad975c9523fd9cf1
Xtal0019 <dmitriyp709@gmail.com>
2025-12-11 10:33:16 +0300

    Добавлены структуры для хранения данных

---
+++ project/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_FILE_ROWS_COUNT 100
+#define MAX_STRING_SIZE 200
+
+#endif
---
+++ project/participant_inf.cpp
@@ -0,0 +1,29 @@
+#ifndef PARTICIPANT_INF_H
+#define PARTICIPANT_INF_H
+
+#include "constants.h"
+
+struct time
+{
+    int hour;
+    int minutes;
+    int seconds;
+};
+
+struct person
+{
+    char first_name[MAX_STRING_SIZE];
+    char middle_name[MAX_STRING_SIZE];
+    char last_name[MAX_STRING_SIZE];
+};
+
+struct participant_inf
+{
+    int numb;
+    person reader;
+    time start;
+    time finish;
+    char club[MAX_STRING_SIZE];
+};
+
+#endif
